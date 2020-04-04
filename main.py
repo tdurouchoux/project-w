78d0568ef9edba5750a723dff137bd196c543f86
tdurouchoux <thibault.durouchoux@gmail.com>
2020-04-04 22:49:42 +0000

    help

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ irSensors = IrSensors()
 command_queue = Queue()
 delta_position_queue = Queue()
 detected_object_queue = Queue()
+other_queue = Queue()
 
 t1 = Thread(target=speedController.run,args=(command_queue,delta_position_queue,))
-t2 = Thread(target=pygameController.run,args=(command_queue,delta_position_queue,detected_object_queue,))
+t2 = Thread(target=pygameController.run,args=(command_queue,delta_position_queue,other_queue,))
 t3 = Thread(target=irSensors.run,args=(detected_object_queue,))
 
 t1.start()
